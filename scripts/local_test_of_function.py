e1ea9e64363d0b684c01f2ab3c35d7112df13c82
ayushijain1198 <ayushi11jain98@gmail.com>
2021-07-06 11:52:43 +0530

    1st commit

--- scripts/local_test_of_function.py
+++ scripts/local_test_of_function.py
@@ -36,13 +36,13 @@ This file will be written to the working directory.
 
 '''
 
-from custom.functions import HelloWorld
+from customayushi.functions import Count1
 
-fn = HelloWorld(name='AS_Tester', greeting_col='greeting')
+fn = Count1(name='AS_Tester', greeting_col='greeting')
 fn.execute_local_test(db=db, db_schema=db_schema)
 
 '''
 Register function so that you can see it in the UI
 '''
 
-db.register_functions([HelloWorld])
+db.register_functions([Count1])
